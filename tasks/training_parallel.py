2527ade473f9eefa251ff63461ad6e0561889335
jindal2309 <gj3bg@virginia.edu>
2020-03-28 01:30:05 -0400

    add tensorboard

--- tasks/training_parallel.py
+++ tasks/training_parallel.py
@@ -10,20 +10,22 @@ import matplotlib.pylab as plt
 import torch
 import torch.nn as nn
 from torch.optim.lr_scheduler import ReduceLROnPlateau, CyclicLR
+from torch.utils.tensorboard import SummaryWriter
 from torchtext import data
 from transformers import GPT2Tokenizer, GPT2LMHeadModel
 import sys
-
+import time
 import logging
 logging.basicConfig(level=logging.INFO)
 
 
 max_len = 20 
 batch_size = 64
-num_epochs = 5
-learning_rate = 0.0001
+num_epochs = 10
+learning_rate = 0.001
 cuda = True
 data_dir = '/u/gj3bg/gj3bg/cornell movie-dialogs corpus/'
+summary_dir = './tb_summary'
 
 # Set device type
 if cuda and torch.cuda.is_available():
@@ -45,18 +47,19 @@ print(convtexts[:1])
 # Load pre-trained model tokenizer (vocabulary)
 tokenizer = GPT2Tokenizer.from_pretrained('gpt2')
 model = GPT2LMHeadModel.from_pretrained('gpt2')
-
+model.to(device)
 
 train_file = data_dir + 'dialogue_training_data.csv'
 valid_file  = data_dir + 'dialogue_validation_data.csv'
 
-# Data Parallelism over 4 GPUs
-if torch.cuda.device_count() > 1:
-    print("Running on ", torch.cuda.device_count(), "GPU's")
-    model = nn.DataParallel(model)
-    model.to(device)
-else:
-    model.to(device)
+# # Data Parallelism over 4 GPUs
+# if torch.cuda.device_count() > 1:
+#     print("Running on ", torch.cuda.device_count(), "GPU's")
+#     model = nn.DataParallel(model)
+#     model.to(device)
+# else:
+#     model.to(device)
+
 
 tokenizer.pad_token = '<PAD>'
 pad_index = tokenizer.convert_tokens_to_ids(tokenizer.pad_token)
@@ -100,13 +103,20 @@ scheduler = CyclicLR(optimizer, base_lr=learning_rate, max_lr=1, mode="exp_range
 model = GPT2LMHeadModel.from_pretrained('gpt2')
 model.to(device)
 
+# Initialize summary writer
+writer = SummaryWriter(summary_dir)
+
 # Start Training
 training_loss_list = []
 validation_loss_list = []
     
 print("-" * 84)
 print("Start Training")
+start_time = time.time()
+
 for epoch in range(num_epochs):
+    epoch_start = time.time()
+    writer.add_scalar("optim/lr", optimizer.param_groups[0]["lr"], epoch)
     model.train()
     training_loss = 0
     for i, batch in enumerate(train_iterator):
@@ -139,6 +149,14 @@ for epoch in range(num_epochs):
         #sys.exit()
     print("epoch", epoch, "training_loss_per_epoch", training_loss)
     training_loss_list.append(training_loss)
+
+    # Add training summary to tensorboard
+    writer.add_scalar("train/loss", training_loss, epoch)
+    for name, params in model.named_parameters():
+        # print( "params.requires_grad : ", params.requires_grad)
+        if params.requires_grad:
+            name = name.replace(".", "/")
+            writer.add_histogram(name, params.data, epoch)
     
     #Evaluation on Validation data
     with torch.no_grad():
@@ -163,7 +181,14 @@ for epoch in range(num_epochs):
                     validation_loss += loss.item()
                 predicted_index = torch.argmax(predictions, dim = 1)
                 tokens_tensor = torch.cat((tokens_tensor, predicted_index.unsqueeze(1)), dim = 1)
+            # Add validation summary to tensorboard
+            # if i == 0:
+            #     for name, value in model.named_parameters():
+            #             name = name.replace(".", "/") + "/grad"
+            #             writer.add_histogram(name, value.grad.data, epoch)
+
         print("epoch", epoch, "validation_loss", validation_loss)
+        writer.add_scalar("valid/loss", validation_loss, epoch)
         validation_loss_list.append(validation_loss)
         # Print the predicted text
         predicted_text = tokenizer.decode(tokens_tensor[0].tolist())
@@ -172,12 +197,13 @@ for epoch in range(num_epochs):
         print("Predicted: ", predicted_text)
         #scheduler.step(validation_loss) 
         # Save loss in text file
-        c = [training_loss_list, validation_loss_list]
+        c = [[i  for i in range(num_epochs)], training_loss_list, validation_loss_list]
         with open("./loss.txt", "a") as file:
             for x in zip(*c):
-                file.write("{0}\t{1}\n".format(*x)) 
+                file.write("{0}\t{1}\t{2}\n".format(*x)) 
         model_file = './saved_model.pkl'
-        #torch.save(model, model_file)  
+        #torch.save(model, model_file)
+        print("Per epoch Training + Validation Time: {:0.2f} mins".format((time.time() - epoch_start) / 60))  
 
 
 plt.figure(figsize = (10, 4))
@@ -189,6 +215,23 @@ plt.ylabel('loss')
 plt.xlabel('epoch')
 plt.legend(['validation', 'training'], loc='upper right')
 plt.savefig("./Loss_vs_epoch.png")
+
+
+# writer.add_hparams(
+#     {
+#         "lr": learning_rate,
+#         "seq-len": max_len,
+#         "batch-size": batch_size,
+#         "num-epochs": num_epochs,
+#     },
+#     {
+#         "train/loss": training_loss,
+#         "val/loss": validation_loss,
+#     },
+# )
+
+print("Total Training + Validation Time: {:0.2f} mins".format((time.time() - start_time) / 60))
+
 
 model_file = './saved_model.pkl'
 #torch.save(model, model_file)
